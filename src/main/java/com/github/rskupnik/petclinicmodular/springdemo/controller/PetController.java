212a365f1fdbfabc644cc8a99b714183104c93ea
Myzreal <rskupnik@gmail.com>
2018-12-07 11:15:57 +0100

    Update routing in pet controller

--- src/main/java/com/github/rskupnik/petclinicmodular/springdemo/controller/PetController.java
+++ src/main/java/com/github/rskupnik/petclinicmodular/springdemo/controller/PetController.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController("/pet")
+@RestController("/pets")
 public class PetController {
 
     private final PetService petService;
