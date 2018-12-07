d2b3cc64fe34689b515e50681b7e1d85e0ee305b
Myzreal <rskupnik@gmail.com>
2018-12-07 11:30:55 +0100

    Add DI wiring and rename the config class

--- src/main/java/com/github/rskupnik/petclinicmodular/springdemo/config/DefaultImplementationConfig.java
+++ src/main/java/com/github/rskupnik/petclinicmodular/springdemo/config/DefaultImplementationConfig.java
@@ -1,14 +1,18 @@
 package com.github.rskupnik.petclinicmodular.springdemo.config;
 
+import com.github.rskupnik.petclinicmodular.customer.repository.CustomerRepository;
+import com.github.rskupnik.petclinicmodular.customer.repository.defaultimpl.DefaultCustomerRepository;
 import com.github.rskupnik.petclinicmodular.pet.repository.PetRepository;
 import com.github.rskupnik.petclinicmodular.pet.repository.defaultimpl.DefaultPetRepository;
+import com.github.rskupnik.petclinicmodular.service.CustomerService;
 import com.github.rskupnik.petclinicmodular.service.PetService;
+import com.github.rskupnik.petclinicmodular.service.defaultimpl.DefaultCustomerService;
 import com.github.rskupnik.petclinicmodular.service.defaultimpl.DefaultPetService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationBeansConfig {
+public class DefaultImplementationConfig {
 
     @Bean
     public PetRepository petRepository() {
@@ -18,5 +22,15 @@ public class ApplicationBeansConfig {
     @Bean
     public PetService petService(PetRepository petRepository) {
         return new DefaultPetService(petRepository);
+    }
+
+    @Bean
+    public CustomerRepository customerRepository() {
+        return new DefaultCustomerRepository();
+    }
+
+    @Bean
+    public CustomerService customerService(CustomerRepository customerRepository) {
+        return new DefaultCustomerService(customerRepository);
     }
 }
